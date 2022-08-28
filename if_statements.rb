9177e948bda54bfacbc499b3b9e4a6016dfdf315
Issa Abdel Mageed <imageed95@gmail.com>
2022-08-28 16:06:32 -0600

    added code for quarters and pizza

--- if_statements.rb
+++ if_statements.rb
@@ -3,7 +3,7 @@
 # file by entering the following command in your Terminal:
 # `ruby if_statements.rb`
 
-# Example: Using the weather variable below, write code that decides 
+# Example: Using the weather variable below, write code that decides
 # what you should take with you based on the following conditions:
   # if it is sunny, print "sunscreen"
   # if it is rainy, print "umbrella"
@@ -35,14 +35,14 @@ end
 
 # Right now, the program will print
 # out both "I have enough money for a gumball" and
-# "I don't have enough money for a gumball". Write a 
+# "I don't have enough money for a gumball". Write a
 # conditional statement that prints only one or the other.
 
 # Experiment with manipulating the value held within num_quarters
 # to make sure both conditions can be achieved.
 
 num_quarters = 2
-if num_quarters >= 2 
+if num_quarters >= 2
 puts "I have enough money for a gumball"
 else
 puts "I don't have enough money for a gumball"
@@ -50,7 +50,7 @@ end
 
 #####################
 # Using the variables defined below, write code that will tell you
-# if you have the ingredients to make a pizza. A pizza requires 
+# if you have the ingredients to make a pizza. A pizza requires
 # at least two cups of flour and sauce.
 
 # You should be able to change the variables to achieve the following outputs:
@@ -64,3 +64,12 @@ end
 
 cups_of_flour = 1
 has_sauce = true
+if cups_of_flour== 1 && has_sauce== true
+  print "i cannot make pizza"
+elsif cups_of_flour ==5 && has_sauce == false
+  print "I cannot make pizza"
+elsif cups_of_flour == 2 && has_sauce == true
+  print "i can make pizza"
+elsif cups_of_flour == 3 && has_sauce == true
+  print "i can make pizza"
+end

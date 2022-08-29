e98fbf79aeb8838665a51fcd55b25480033c5eb0
Issa Abdel Mageed <imageed95@gmail.com>
2022-08-28 22:19:43 -0600

    redo of coding challenge

--- code_challenges/code_challenges_work.rb
+++ code_challenges/code_challenges_work.rb
@@ -1,22 +1,19 @@
-puts "Please enter a number:"
- input = Integer(gets)
- if input % 2 == 0
- puts "even"
- else
- puts "odd"
- end
+#puts "please enter a number!"
+#input = Integer(gets)
+#if input % 2 == 0
+#  puts "even"
+#else
+#  puts "odd"
+#end
 
 
-
-
-
-
-puts "please enter a number"
+puts "please enter a number!"
 input = Integer(gets)
- if input % 3 == 0 and input % 5 == 0
-         print("fizzbuzz")
-     elsif input % 3 == 0
-         print("fizz")
-     elsif input % 5 == 0
-         print("buzz")
-   end
+if input % 3 == 0 and input % 5 == 0
+  puts "fizbuzz"
+elsif input % 3 == 0
+  puts "fizz"
+elsif input % 5 == 0
+  puts "buzz"
+else print input
+end

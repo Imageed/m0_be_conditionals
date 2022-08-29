80321ec0884fa6021d630e6f2dfab9d421bf5107
Issa Abdel Mageed <imageed95@gmail.com>
2022-08-28 21:05:39 -0600

    added code challenges work

---
+++ code_challenges/code_challenges_work.rb
@@ -0,0 +1,17 @@
+#puts "Please enter a number:"
+# input = Integer(gets)
+ #if input % 2 == 0
+ #puts "even"
+ #else
+ #puts "odd"
+ #end
+
+puts "please enter a number"
+input = Integer(gets)
+ if input % 3 == 0 and input % 5 == 0
+         print("fizzbuzz") 
+     elsif input % 3 == 0
+         print("fizz")
+     elsif input % 5 == 0
+         print("buzz")
+   end
